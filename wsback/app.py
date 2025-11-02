726421b8031be82dcd4950d73c0e69168d710dc4
YOSRcharek <yosryassoura31@gmail.com>
2025-11-02 03:50:57 +0100

    crud collect

--- wsback/app.py
+++ wsback/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from routes.dechets import dechets_bp
-from routes.event import evenement_bp
-from routes.campagne import campagne_bp
+from routes.events.event import evenement_bp
+from routes.events.collect import collecte_bp
+from routes.events.formation import formation_bp
+from routes.compagne.campagne import campagne_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(evenement_bp)
+app.register_blueprint(collecte_bp)
+app.register_blueprint(formation_bp)
 app.register_blueprint(campagne_bp)
 
 # Enregistrement des Blueprints

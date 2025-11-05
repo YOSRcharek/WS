ad1ee8186186608f92465d74cf69525bb394559d
Aziz-Belhadj <aziz.belhadj@esprit.tn>
2025-11-05 03:14:07 +0100

    resolve conflicts

--- wsback/app.py
+++ wsback/app.py
@@ -1,13 +1,9 @@
 from flask import Flask
-<<<<<<< HEAD
 from flask_cors import CORS
 from routes.dechets.dechets import dechets_bp
 
 from routes.typeDechets.typeDechets import typedechets_bp
 from routes.aidechet import iadechet_bp
-=======
-from routes.dechets.dechets import dechets_bp
->>>>>>> doua
 from routes.events.event import evenement_bp
 from routes.events.collect import collecte_bp
 from routes.events.formation import formation_bp
@@ -15,11 +11,8 @@ from routes.compagne.campagne import campagne_bp
 from routes.compagne.reseaux import reseaux_bp
 from routes.compagne.affiche import affiche_bp
 from routes.ia_sparql import ia_bp   
-<<<<<<< HEAD
 from routes.centres.centres import centres_bp
 from routes.points_collecte.points_collecte import points_collecte_bp
-=======
->>>>>>> doua
 from flask_cors import CORS
 
 # Import des équipements depuis le dossier equipements
@@ -33,24 +26,18 @@ from routes.equipements.conteneur import conteneur_bp
 from routes.services_transport.service_transport import service_transport_bp
 from routes.services_transport.camion_dechets import camion_dechets_bp
 from routes.services_transport.transport_dechets_dangereux import transport_dechets_dangereux_bp
-<<<<<<< HEAD
 #citoyen and municipalite
 from routes.municipalites.municipalite import municipalite_bp
 from routes.citoyens.citoyens import citoyen_bp
 from routes.citizen_matching_ai import citizen_matching_bp
 from routes.citizen_requests import citizen_requests_bp
 
-app = Flask(__name__)
-CORS(app)
-# Enregistrement des Blueprints
 
 
-=======
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3006"], supports_credentials=True)
 # Enregistrement des Blueprints
->>>>>>> doua
 app.register_blueprint(evenement_bp)
 app.register_blueprint(collecte_bp)
 app.register_blueprint(formation_bp)
@@ -69,7 +56,6 @@ app.register_blueprint(conteneur_bp, url_prefix='/api')
 # Transport service subclasses
 app.register_blueprint(camion_dechets_bp, url_prefix='/api')
 app.register_blueprint(transport_dechets_dangereux_bp, url_prefix='/api')
-<<<<<<< HEAD
 app.register_blueprint(municipalite_bp, url_prefix='/api')
 app.register_blueprint(citoyen_bp)
 
@@ -83,12 +69,6 @@ app.register_blueprint(iadechet_bp)
 
 app.register_blueprint(citizen_matching_bp)
 app.register_blueprint(citizen_requests_bp) 
-=======
-
-# Enregistrement des Blueprints
-app.register_blueprint(dechets_bp)
-app.register_blueprint(ia_bp) 
->>>>>>> doua
 
 if __name__ == "__main__":
     app.run(debug=True)
